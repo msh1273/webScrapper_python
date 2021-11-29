d57aa0f030ea5bca74fa35e303f725b16fd3f7dd
msh1273 <mch1273@naver.com>
2021-11-30 03:47:46 +0900

    add flask (home, report)page

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 from indeed import get_jobs as get_indeed_jobs
 from stackof import get_jobs as get_stackof_jobs
 from save import save_to_file
+from flask import Flask, render_template, request
+
+app = Flask("Scrapper")
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/report")
+def report():
+    word = request.args.get('word')
+    return render_template("report.html", searchingBy=word)
+
+
+app.run()
 
 
 indeed_jobs = get_indeed_jobs()
